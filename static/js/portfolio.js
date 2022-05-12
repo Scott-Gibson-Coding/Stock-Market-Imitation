0b425c334d31d7dcd6191d0a1771e6d1588c23c6
Scott Gibson <scottchgibson@gmail.com>
2022-05-12 15:03:14 -0700

    Add vue js files for each page. Only has a single variable at present.

---
+++ static/js/portfolio.js
@@ -0,0 +1,44 @@
+// This will be the object that will contain the Vue attributes
+// and be used to initialize it.
+let app = {};
+
+
+// Given an empty app object, initializes it filling its attributes,
+// creates a Vue instance, and then initializes the Vue instance.
+let init = (app) => {
+
+    // This is the Vue data.
+    app.data = {
+        proof: "Portfolio Proof",
+    };
+
+    app.enumerate = (a) => {
+        // This adds an _idx field to each element of the array
+        let k = 0;
+        a.map((e) => {e._idx = k++;});
+        return a;
+    };
+
+
+    // This contains all the methods
+    app.methods = {
+        
+    };
+
+    // This creates the Vue instance
+    app.vue = new Vue({
+        el: "#vue-target",
+        data: app.data,
+        methods: app.methods
+    });
+
+    app.init = () => {
+        // Put here any initialization code
+    };
+
+    // Call to the initializer
+    app.init();
+};
+
+// Initialize the app object
+init(app);

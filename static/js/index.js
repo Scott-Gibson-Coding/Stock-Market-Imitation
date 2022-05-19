991f1928642c36b99db6a4aa31e3cbd4a3206365
Scott Gibson <scottchgibson@gmail.com>
2022-05-19 14:46:41 -0700

    Add index_layout file, create signup/login form on index page -(TODO) Failure to login or signup doesn't show an error message for the user.

--- static/js/index.js
+++ static/js/index.js
@@ -9,7 +9,14 @@ let init = (app) => {
 
     // This is the Vue data.
     app.data = {
-        proof: "Index Proof",
+        // sign up fields
+        sign_up_email: "",
+        sign_up_first_name: "",
+        sign_up_last_name: "",
+        sign_up_password: "",
+        // log in fields
+        log_in_email: "",
+        log_in_password: "",
     };
 
     app.enumerate = (a) => {
@@ -18,11 +25,51 @@ let init = (app) => {
         a.map((e) => {e._idx = k++;});
         return a;
     };
+    
+    app.login = function() {
+        if (app.vue.log_in_email != "" && app.vue.log_in_password != "") {
+            let self = this;
+            axios.post(login_url,
+                {
+                    email: app.vue.log_in_email,
+                    password: app.vue.log_in_password,
+                }).then(function (response) {
+                    console.log('login attempt successful');
+                    window.location = index_url;
+                }).catch(function (error) {
+                    console.log('an error occured');
+                    console.log(error);
+                });
+        }
+    };
 
+    app.signup = function(props) {
+        if (app.vue.sign_up_email != "" && app.vue.sign_up_first_name != ""
+            && app.vue.sign_up_last_name != "" && app.vue.sign_up_password != "") {
+
+            axios.post(sign_up_url,
+                {
+                    email: app.vue.sign_up_email,
+                    first_name: app.vue.sign_up_first_name,
+                    last_name: app.vue.sign_up_last_name,
+                    password: app.vue.sign_up_password,
+                }
+            ).then(function (response) {
+                console.log('sign up attempt successful');
+                app.vue.log_in_email = app.vue.sign_up_email;
+                app.vue.log_in_password = app.vue.sign_up_password;
+                app.login();
+            }).catch(function (error) {
+                console.log('an error occured');
+                console.log(error);
+            });
+        }
+    };
 
     // This contains all the methods
     app.methods = {
-        
+        login: app.login,
+        signup: app.signup,
     };
 
     // This creates the Vue instance

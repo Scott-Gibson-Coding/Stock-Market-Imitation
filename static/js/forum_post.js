192c864e246900fc630c6361032efaeb7bc371b9
Colin Harmon <coharmon@ucsc.edu>
2022-06-02 21:58:50 -0700

    First draft of system allowing replies to comments

--- static/js/forum_post.js
+++ static/js/forum_post.js
@@ -24,12 +24,23 @@ let init = (app) => {
     };
 
     app.complete = (a) => {
-        // Add reaction for thumbs
+        // Add reactivity elements
         a.map((e) => {
-            e.reaction = 0;
+            //e.reaction = 0;
+            e.adding_reply = false;
+            e.current_reply = "";
+            e.show_replies = false;
         });
         return a
     };
+
+    app.complete_local_comment = (c) => {
+        // Add reactivity elements
+        c.likes = 0;
+        c.dislikes = 0;
+        c.replies = 0;
+        c.reply_list = [];
+    }
 
     app.start_comment = function(){
         // Open field
@@ -43,10 +54,10 @@ let init = (app) => {
     };
 
     app.post_comment = function(){
-        // Send the post to the server if not empty
+        // Send the comment to the server if not empty
         if (app.vue.current_comment != ""){
             let original_text = app.vue.current_comment;
-            axios.post(post_comment_url, {'comment_text' : original_text}
+            axios.post(post_comment_url, {'comment_text' : original_text, 'parent_idx': -1}
             ).then(function(response){
                 // Add to reactive list
                 //console.log(response.data);
@@ -55,8 +66,7 @@ let init = (app) => {
                 new_comment.comment = original_text;
                 // Add reactive variables to the new post
                 app.complete([new_comment]);
-                new_comment['likes'] = 0;
-                new_comment['dislikes'] = 0;
+                app.complete_local_comment(new_comment);
                 // Place at front of list and redo indices
                 app.vue.comment_list.unshift(new_comment);
                 app.enumerate(app.vue.comment_list);
@@ -103,6 +113,89 @@ let init = (app) => {
         );
     };
 
+    app.start_reply = function(comment_idx){
+        let comment = app.vue.comment_list[comment_idx];
+        comment.adding_reply = true;
+    };
+
+    app.cancel_reply = function(comment_idx){
+        let comment = app.vue.comment_list[comment_idx];
+        comment.adding_reply = false;
+        comment.current_reply = "";
+    };
+
+    app.post_reply = function(comment_idx){
+        // Send the comment to the server if not empty
+        let comment = app.vue.comment_list[comment_idx];
+        if (comment.current_reply != ""){
+            let original_text = comment.current_reply;
+            axios.post(post_comment_url, {'comment_text' : original_text, 'parent_idx':comment.id}
+            ).then(function(response){
+                // Add to reactive list
+                let new_comment = Object.assign({}, response.data);
+                delete new_comment.flash;
+                new_comment.comment = original_text;
+                // Add reactive variables to the new post
+                new_comment['likes'] = 0;
+                new_comment['dislikes'] = 0;
+                app.cancel_reply(comment_idx);
+                // Place at back of list and redo indices
+                comment.reply_list.push(new_comment);
+                app.enumerate(comment.reply_list);
+            });
+        }
+    };
+
+    app.display_replies = function(comment_idx){
+        let comment = app.vue.comment_list[comment_idx];
+        comment.show_replies = true;
+    };
+
+    app.hide_replies = function(comment_idx){
+        let comment = app.vue.comment_list[comment_idx];
+        comment.show_replies = false;
+    };
+
+    app.delete_reply = function(comment_idx, reply_idx){
+        let comment = app.vue.comment_list[comment_idx];
+        let reply = comment.reply_list.splice(reply_idx, 1)[0];
+        app.enumerate(comment.reply_list);
+        axios.get(delete_comment_url, {'params':{'comment_id':reply.id}});
+    };
+
+    app.react_reply = function(comment_idx, reply_idx, react){
+        let comment = app.vue.comment_list[comment_idx];
+        let reply = comment.reply_list[reply_idx];
+        // Change the reaction state
+        if (react === 'up'){
+            if (reply.reaction === 1){
+                reply.reaction = 0;
+                reply.likes--;
+            } else{
+                if (reply.reaction === -1){
+                    reply.dislikes--;
+                }
+                reply.reaction = 1;
+                reply.likes++;
+            }
+        } else if (react === 'down'){
+            if (reply.reaction === -1){
+                reply.reaction = 0;
+                reply.dislikes--;
+            } else{
+                if (reply.reaction === 1){
+                    reply.likes--;
+                }
+                reply.reaction = -1;
+                reply.dislikes++;
+            }
+        }
+        // Save the new reaction to the db
+        axios.post(save_reaction_url, 
+            {'comment_id' : reply.id, 'reaction' : reply.reaction}
+        );
+    };
+
     // This contains all the methods
     app.methods = {
         start_comment: app.start_comment,
@@ -110,6 +203,13 @@ let init = (app) => {
         post_comment: app.post_comment,
         delete_comment: app.delete_comment,
         react: app.react,
+        start_reply: app.start_reply,
+        cancel_reply: app.cancel_reply,
+        post_reply : app.post_reply,
+        display_replies : app.display_replies,
+        hide_replies : app.hide_replies,
+        delete_reply : app.delete_reply,
+        react_reply : app.react_reply,
     };
 
     // This creates the Vue instance
@@ -126,18 +226,14 @@ let init = (app) => {
             // Save the comments
             let comments = response.data.comments;
             app.enumerate(comments);
+            for (let c of comments){
+                app.enumerate(c.reply_list);
+            }
             app.complete(comments);
             app.vue.comment_list = comments;
             // Save current user name and email
             app.vue.current_user_name = response.data.current_user_name;
             app.vue.current_user_email = response.data.current_user_email;
-            // Get reactions
-            let reactions = response.data.reactions;
-            // Pair with correct comment
-            for (let react of reactions){
-                let c = app.vue.comment_list.find(el=>el.id === react.comment_id);
-                c.reaction = react.reaction;
-            }
         });
     };
 

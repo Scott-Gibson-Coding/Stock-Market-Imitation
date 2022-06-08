865a319dddf6ba00e3d99392adb4ed22e09983c5
Ryan <leungryan26@gmail.com>
2022-06-08 11:26:11 -0700

    get net worth history function

--- utilities.py
+++ utilities.py
@@ -3,6 +3,9 @@ Here we can put functions which are imported and used by controllers.py
 This way we don't have to clutter the controllers.py file with function
 declarations that are unrelated to hooking pages to URLs
 """
+import datetime
+
+from .StockSimulator import StockSimulator
 from .common import db
 
 def get_portfolio(user_id:int) -> dict:
@@ -47,3 +50,52 @@ def get_avg_bought_price(user_id, company_id):
         count += r.count
         sum += r.count * r.value_per_share
     return sum / count
+
+def get_net_worth_history(user_id, time=None, steps=30):
+    if time is None:
+        time = datetime.datetime.utcnow() - datetime.timedelta(minutes=5)
+    
+    def interval():
+        delta = datetime.datetime.utcnow() - time
+        delta /= steps - 1
+        for i in range(steps):
+            yield time + (delta * i)
+    
+    history = []
+    sim = StockSimulator()
+    holdings = {}
+    balance = 10000
+    transactions = db(db.transaction.user_id == user_id).select(orderby=db.transaction.transaction_date)
+    transactions = transactions.as_list()
+    pos = 0
+
+    def advance_transactions(t):
+        nonlocal pos, balance
+        while pos < len(transactions) and (row := transactions[pos])['transaction_date'] < t:
+            pos += 1
+            if row['transaction_type'] == 'buy':
+                if row['company_id'] not in holdings:
+                    holdings[row['company_id']] = 0.0
+                holdings[row['company_id']] += row['count']
+                balance -= row['count'] * row['value_per_share']
+            else:
+                if row['company_id'] not in holdings:
+                    raise KeyError('Attempted to sell a stock the user does not own')
+                if row['count'] > holdings[row['company_id']]:
+                    raise ValueError(f'Cannot sell {row["count"]} shares, user owns {holdings[row["company_id"]]}')
+                holdings[row['company_id']] -= row['count']
+                balance += row['count'] * row['value_per_share']
+
+    advance_transactions(time)
+
+    for timestep in interval():
+        advance_transactions(timestep)
+        net_worth = balance
+        for k,v in holdings.items():
+            price = sim.load_company(k, timestep)['current_stock_value']
+            net_worth += price * v
+        history.append(net_worth)
+    
+    return history
+    
+    
